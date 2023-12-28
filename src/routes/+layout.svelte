8d27e271f417fac1b7fb98e4c2c0e38e961f0634
Buotangbata <103356537+Buotangbata@users.noreply.github.com>
2023-12-28 12:26:50 +0800

    initializing and css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import "../app.css";
+</script>
+
+<h1 class="text-3xl font-bold underline">
+    I am layout
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
+
+<slot/>
